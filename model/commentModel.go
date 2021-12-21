9993c11a86c55cb3bcf30c4d265d5a911a45470f
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-12-21 22:54:57 +0700

    Add

--- model/commentModel.go
+++ model/commentModel.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
@@ -9,9 +11,31 @@ type Comment struct {
 	GormModel
 	Message string `json:"message" form:"message" valid:"required~Message of your comment is required"`
 	UserID  uint
-	PhotoID uint
+	PhotoID uint `json:"photo_id" form:"photo_id"`
 	User    *User
 	Photo   *Photo
+}
+
+type GetPhotoComment struct {
+	ID        uint       `json:"id"`
+	Message   string     `json:"message"`
+	UserID    uint       `json:"user_id"`
+	CreatedAt *time.Time `json:"created_at"`
+}
+
+type MyComment struct {
+	ID        uint       `json:"id"`
+	Message   string     `json:"message"`
+	PhotoID   uint       `json:"photo_id"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	CreatedAt *time.Time `json:"created_at"`
+	Photo     struct {
+		ID        uint   `json:"id"`
+		Title     string `json:"titile"`
+		Caption   string `json:"caption"`
+		Photo_url string `json:"photo_url"`
+		UserID    uint   `json:"user_id"`
+	} `json:"photos"`
 }
 
 func (c *Comment) BeforeCreate(tx *gorm.DB) (err error) {
