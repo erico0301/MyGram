b97489ae0dcb49935661005f36a513eae4ebad36
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-12-22 23:30:30 +0700

    Update db.go

--- database/db.go
+++ database/db.go
@@ -4,31 +4,30 @@ import (
 	"MyGram/model"
 	"fmt"
 	"log"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// var (
-// 	host     = "localhost"
-// 	user     = "postgres"
-// 	password = "1234"
-// 	dbPort   = "5432"
-// 	dbName   = "mygram"
-// 	db       *gorm.DB
-// 	err      error
-// )
-
 var (
-	host     = os.Getenv("DB_HOST")
-	user     = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	dbPort   = os.Getenv("DB_PORT")
-	dbName   = os.Getenv("DB_NAME")
+	host     = "mygram.cygn9doqbcyr.ap-southeast-1.rds.amazonaws.com"
+	user     = "postgres"
+	password = "1234"
+	dbPort   = "5432"
+	dbName   = "mygram"
 	db       *gorm.DB
 	err      error
 )
+
+// var (
+// 	host     = os.Getenv("DB_HOST")
+// 	user     = os.Getenv("DB_USER")
+// 	password = os.Getenv("DB_PASSWORD")
+// 	dbPort   = os.Getenv("DB_PORT")
+// 	dbName   = os.Getenv("DB_NAME")
+// 	db       *gorm.DB
+// 	err      error
+// )
 
 func StartDB() {
 	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
